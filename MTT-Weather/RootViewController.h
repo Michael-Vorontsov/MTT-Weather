ee1130ed4c6616e364e91f8d3492a0810faeb68c
Mykhailo Vorontsov <mvorontsov@MICHEL-AIR.local>
2014-10-25 16:50:51 +0300

    Initial Commit

---
+++ MTT-Weather/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  RootViewController.h
+//  MTT-Weather
+//
+//  Created by Mykhailo Vorontsov on 10/25/14.
+//  Copyright (c) 2014 MTT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RootViewController : UIViewController <UIPageViewControllerDelegate>
+
+@property (strong, nonatomic) UIPageViewController *pageViewController;
+
+@end
+
