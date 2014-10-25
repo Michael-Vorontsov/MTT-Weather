f857e13be3177d77c870ca4d11c50d5ddc0f8906
Mykhailo Vorontsov <mvorontsov@MICHEL-AIR.local>
2014-10-25 20:12:20 +0300

    Add Core Data Controller and OCMock framework

---
+++ MTT-Weather/Sources/DOM/MTTWDataController.h
@@ -0,0 +1,13 @@
+//
+//  MTTWDataController.h
+//  MTT-Weather
+//
+//  Created by Mykhailo Vorontsov on 10/25/14.
+//  Copyright (c) 2014 MTT. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MTTWDataController : NSObject
+
+@end
---
+++ MTT-Weather/Sources/DOM/MTTWDataController.m
@@ -0,0 +1,13 @@
+//
+//  MTTWDataController.m
+//  MTT-Weather
+//
+//  Created by Mykhailo Vorontsov on 10/25/14.
+//  Copyright (c) 2014 MTT. All rights reserved.
+//
+
+#import "MTTWDataController.h"
+
+@implementation MTTWDataController
+
+@end
